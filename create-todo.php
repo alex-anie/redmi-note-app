70f2367fdb96e692d209ff4fc8841c7742a40959
alex-anie <ocxigin@gmail.com>
2025-04-25 19:24:57 +0100

    create the create-todo.php file

---
+++ create-todo.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Create todo</title>
+    <link rel="stylesheet" href="css/main.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+</head>
+<body>
+    <main class="website-site">
+        <article>
+            <form action="" method="post">
+                <div class="input-title">
+                    <label class="heading-label">What are you doing today?</label>
+                    <input type="text" id="text" name="text" placeholder="Title..." value="">
+                </div>
+                <div class="text-body">
+                    <textarea name="text" id="text" rows="15" cols="33">Create a Todo...</textarea>
+                </div>
+
+                <button class="btn-submit">
+                    <i class="fa-solid fa-paper-plane"></i>
+                </button>
+            </form>
+
+        </article>
+    </main>
+</body>
+</html>
